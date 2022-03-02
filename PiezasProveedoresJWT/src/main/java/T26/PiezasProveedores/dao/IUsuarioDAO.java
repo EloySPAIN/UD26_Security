8c57ac2b47b74b58c3063c568e458ed5e3005501
JoanR17 <joanr.m.1999@gmail.com>
2022-03-02 10:47:21 +0100

    Ej 1

--- PiezasProveedoresJWT/src/main/java/T26/PiezasProveedores/dao/IUsuarioDAO.java
+++
@@ -1,9 +0,0 @@
-package T26.PiezasProveedores.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import T26.PiezasProveedores.dto.Usuario;
-
-public interface IUsuarioDAO extends JpaRepository<Usuario, Long> {
-
-	Usuario findByUsername(String username);
-}
