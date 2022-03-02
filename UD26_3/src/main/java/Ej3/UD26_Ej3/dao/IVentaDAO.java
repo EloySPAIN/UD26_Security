2eb36793d9a20afae2277645003284f5763367b7
Jordi Ribellas <ribellas2002@gmail.com>
2022-03-02 09:49:17 +0100

    Delete UD26_3 directory

--- UD26_3/src/main/java/Ej3/UD26_Ej3/dao/IVentaDAO.java
+++
@@ -1,8 +0,0 @@
-package Ej3.UD26_Ej3.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import Ej3.UD26_Ej3.dto.Venta;
-
-public interface IVentaDAO extends JpaRepository<Venta, Integer>{
-
-}
